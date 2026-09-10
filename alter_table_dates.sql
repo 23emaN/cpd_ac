1e276319c78c383497f8211db7dc2a8429d473b2
23emaN <panemoboy@gmail.com>
2026-09-10 16:56:02 +0700

    deploy.yml

--- alter_table_dates.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE tbl_customer_work_periods
-ADD COLUMN doc_date DATE DEFAULT NULL COMMENT 'วันที่ได้รับเอกสาร',
-ADD COLUMN completed_date DATE DEFAULT NULL COMMENT 'วันที่ทำเสร็จ',
-ADD COLUMN tax_date DATE DEFAULT NULL COMMENT 'วันที่ยื่นภาษี';
